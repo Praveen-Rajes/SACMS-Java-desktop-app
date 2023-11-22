95d57371f7c96902fc7a3852ed1babf80cf8c410
ThiseniSenethma <thisenisenethma@gmail.com>
2023-11-22 08:02:15 +0530

    edited studentfxml

--- OOD/src/main/java/com/example/ood/StudentRegistration.java
+++ OOD/src/main/java/com/example/ood/StudentRegistration.java
@@ -2,11 +2,11 @@ package com.example.ood;
 
 public class StudentRegistration extends Person {
     private int studentId;
-    private String studentFirstName;
-    private String studentLastName;
-    private String studentDOB;
-    private String studentGender;
-    private String studentAddress;
+//    private String studentFirstName;
+//    private String studentLastName;
+//    private String studentDOB;
+//    private String studentGender;
+//    private String studentAddress;
     private String studentGradeClass;
     private GuardianDetails GuardianDetails;
 
@@ -24,45 +24,45 @@ public class StudentRegistration extends Person {
         this.studentId = studentId;
     }
 
-    public String getStudentFirstName() {
-        return studentFirstName;
-    }
-
-    public void setStudentFirstName(String studentFirstName) {
-        this.studentFirstName = studentFirstName;
-    }
-
-    public String getStudentLastName() {
-        return studentLastName;
-    }
-
-    public void setStudentLastName(String studentLastName) {
-        this.studentLastName = studentLastName;
-    }
-
-    public String getStudentDOB() {
-        return studentDOB;
-    }
-
-    public void setStudentDOB(String studentDOB) {
-        this.studentDOB = studentDOB;
-    }
-
-    public String getStudentGender() {
-        return studentGender;
-    }
-
-    public void setStudentGender(String studentGender) {
-        this.studentGender = studentGender;
-    }
-
-    public String getStudentAddress() {
-        return studentAddress;
-    }
-
-    public void setStudentAddress(String studentAddress) {
-        this.studentAddress = studentAddress;
-    }
+//    public String getStudentFirstName() {
+//        return studentFirstName;
+//    }
+//
+//    public void setStudentFirstName(String studentFirstName) {
+//        this.studentFirstName = studentFirstName;
+//    }
+//
+//    public String getStudentLastName() {
+//        return studentLastName;
+//    }
+//
+//    public void setStudentLastName(String studentLastName) {
+//        this.studentLastName = studentLastName;
+//    }
+//
+//    public String getStudentDOB() {
+//        return studentDOB;
+//    }
+//
+//    public void setStudentDOB(String studentDOB) {
+//        this.studentDOB = studentDOB;
+//    }
+//
+//    public String getStudentGender() {
+//        return studentGender;
+//    }
+//
+//    public void setStudentGender(String studentGender) {
+//        this.studentGender = studentGender;
+//    }
+//
+//    public String getStudentAddress() {
+//        return studentAddress;
+//    }
+//
+//    public void setStudentAddress(String studentAddress) {
+//        this.studentAddress = studentAddress;
+//    }
 
     public String getStudentGradeClass() {
         return studentGradeClass;
@@ -72,6 +72,7 @@ public class StudentRegistration extends Person {
         this.studentGradeClass = studentGradeClass;
     }
 
+    //unique getId methd for each class
     @Override
     public int getId() {
         return getStudentId();
