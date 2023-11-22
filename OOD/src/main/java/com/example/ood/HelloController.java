78f5279c12321c6512f82794970ba67ab6fbdb52
Praveen-Rajes <praveen.20220268@iit.ac.lk>
2023-11-22 13:56:13 +0530

    advisor login

--- OOD/src/main/java/com/example/ood/HelloController.java
+++ OOD/src/main/java/com/example/ood/HelloController.java
@@ -1,8 +1,38 @@
 package com.example.ood;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 
 public class HelloController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
 
+
+    public void  switchStudent (ActionEvent event) throws IOException {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1200, 750);
+
+        stage.setScene(scene);
+        stage.show();
+
+    }
+    public void  switchAdvisor (ActionEvent event) throws IOException {
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("advisor_login.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+
+    }
 }
