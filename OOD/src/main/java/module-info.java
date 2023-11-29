6ffefc37686e23d1c31dd13d532088d9c01ef066
Praveen-Rajes <praveen.20220268@iit.ac.lk>
2023-11-29 13:27:53 +0530

    Calender view + report genraation

--- OOD/src/main/java/module-info.java
+++ OOD/src/main/java/module-info.java
@@ -5,6 +5,7 @@ module com.example.ood {
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.ikonli.fontawesome5;
     requires java.sql;
+    requires java.desktop;
 
 
     opens com.example.ood to javafx.fxml;
